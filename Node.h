7168f12115b142bcd1b471f820eec49fc3727fdd
JONATHAN HAGEN <jhagen920110@gmail.com>
2017-01-29 00:49:06 -0800

    Node header file

---
+++ Node.h
@@ -0,0 +1,24 @@
+class Node
+{
+public:
+	char name;
+    int index;
+	Node *pred;
+	double weight;
+	char set; //Q or S
+	Node::Node()
+	{name='?';
+	 index=0;
+	 pred='\0';
+	 weight=10000;
+	 set='Q';
+	}
+
+	Node::Node(char in_name, int n)
+	{name=in_name;
+	 weight=10000;
+	 pred='\0';
+	 set='Q';
+	 index=n;
+	}
+};
